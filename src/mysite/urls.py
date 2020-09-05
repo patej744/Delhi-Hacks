80353340921c47395211417ca38e49cceab6f322
amodi <ayush.modi.19@ucl.ac.uk>
2020-09-05 16:08:55 +0100

    changed urlpatterns

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('homepage/', include('product.urls'))
+    path("", include('product.urls'))
 ]
